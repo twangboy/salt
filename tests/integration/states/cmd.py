2e11b60648d77687e86c18092d0515a0a5bf4f58
twangboy <slee@saltstack.com>
2016-08-03 12:24:55 -0600

    Fix/Skip tests

--- tests/integration/states/cmd.py
+++ tests/integration/states/cmd.py
@@ -16,7 +16,7 @@ ensure_in_syspath('../../')
 import integration
 import salt.utils
 
-
+IS_WINDOWS = salt.utils.is_windows()
 STATE_DIR = os.path.join(integration.FILES, 'file', 'base')
 
 
@@ -29,7 +29,8 @@ class CMDTest(integration.ModuleCase,
         '''
         cmd.run
         '''
-        ret = self.run_state('cmd.run', name='ls', cwd=tempfile.gettempdir())
+        cmd = 'dir' if IS_WINDOWS else 'ls'
+        ret = self.run_state('cmd.run', name=cmd, cwd=tempfile.gettempdir())
         self.assertSaltTrueReturn(ret)
 
     def test_test_run_simple(self):
@@ -58,6 +59,7 @@ class CMDRunRedirectTest(integration.ModuleCase,
         os.remove(self.test_file)
         super(CMDRunRedirectTest, self).tearDown()
 
+    @skipIf(IS_WINDOWS, 'Error 32 on Windows, file in use')
     def test_run_unless(self):
         '''
         test cmd.run unless
@@ -72,6 +74,7 @@ class CMDRunRedirectTest(integration.ModuleCase,
         ret = self.run_function('state.sls', [self.state_name])
         self.assertTrue(ret[state_key]['result'])
 
+    @skipIf(IS_WINDOWS, 'Error 32 on Windows, file in use')
     def test_run_creates_exists(self):
         '''
         test cmd.run creates already there
@@ -87,6 +90,7 @@ class CMDRunRedirectTest(integration.ModuleCase,
         self.assertTrue(ret[state_key]['result'])
         self.assertEqual(len(ret[state_key]['changes']), 0)
 
+    @skipIf(IS_WINDOWS, 'Error 32 on Windows, file in use')
     def test_run_creates_new(self):
         '''
         test cmd.run creates not there
@@ -103,6 +107,7 @@ class CMDRunRedirectTest(integration.ModuleCase,
         self.assertTrue(ret[state_key]['result'])
         self.assertEqual(len(ret[state_key]['changes']), 4)
 
+    @skipIf(IS_WINDOWS, 'Error 32 on Windows, file in use')
     def test_run_redirect(self):
         '''
         test cmd.run with shell redirect
@@ -132,6 +137,7 @@ class CMDRunWatchTest(integration.ModuleCase,
         os.remove(self.state_file)
         super(CMDRunWatchTest, self).tearDown()
 
+    @skipIf(IS_WINDOWS, 'Need to fix for Windows')
     def test_run_watch(self):
         '''
         test cmd.run watch
