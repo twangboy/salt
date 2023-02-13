58c094ee20a567e9e6e402c4105a3bbb6a5d6383
Megan Wilhite <mwilhite@vmware.com>
2023-02-13 12:10:32 -0700

    Set correct permissions for directory rsyncd

--- tools/vm.py
+++ tools/vm.py
@@ -1004,12 +1004,25 @@ class VM:
         source = f"{REPO_ROOT}{os.path.sep}"
         # Remote repo path
         remote_path = self.upload_path.as_posix()
+        rsync_remote_path = remote_path
         if self.is_windows:
             for drive in ("c:", "C:"):
-                remote_path = remote_path.replace(drive, "/cygdrive/c")
-        destination = f"{self.name}:{remote_path}"
+                rsync_remote_path = rsync_remote_path.replace(drive, "/cygdrive/c")
+        destination = f"{self.name}:{rsync_remote_path}"
         description = "Rsync local checkout to VM..."
         self.rsync(source, destination, description, rsync_flags)
+        if self.is_windows:
+            # Need to set the permissions correctly. Rsync does not
+            # set correct permissions
+            cmd = ["icacls", remote_path, "/T", "/reset"]
+            ret = self.run(cmd, capture=True, check=False)
+            if ret.returncode != 0:
+                self.ctx.exit(ret.returncode, ret.stderr.strip())
+
+            cmd = ["icacls", remote_path, "/grant", "Administrator:(OI)(CI)F", "/T"]
+            ret = self.run(cmd, capture=True, check=False)
+            if ret.returncode != 0:
+                self.ctx.exit(ret.returncode, ret.stderr.strip())
 
     def write_and_upload_dot_env(self, env: dict[str, str]):
         if not env:
@@ -1045,7 +1058,8 @@ class VM:
             self.ctx.exit(1, message=f"{self!r} is not running")
         if env is None:
             env = []
-        env.append("PYTHONUTF8=1")
+        if self.is_windows is False:
+            env.append("PYTHONUTF8=1")
         self.write_ssh_config()
         try:
             ssh_command = self.ssh_command_args(
